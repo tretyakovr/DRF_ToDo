4822dbda8aa0d36254d30e4d19d7db650b8ed9cc
tretyakovr <rtretyakov@outlook.com>
2021-11-17 07:28:08 +1000

    ДЗ № 1

--- DRF_ToDo/users/management/commands/create_users.py
+++ DRF_ToDo/users/management/commands/create_users.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from users.models import Users
-from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
--- DRF_ToDo/users/serializers.py
+++ DRF_ToDo/users/serializers.py
@@ -3,7 +3,6 @@ from .models import Users
 
 
 class UsersModelSerializer(HyperlinkedModelSerializer):
-   class Meta:
-       model = Users
-       fields = ('username', 'first_name', 'last_name', 'email')
-
+    class Meta:
+        model = Users
+        fields = ('username', 'first_name', 'last_name', 'email')
